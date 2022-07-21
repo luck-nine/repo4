55a66a735ecb38e03c05b5fae3e0bf28565729a7
luck <1506024313@qq.com>
2022-07-21 12:18:18 +0800

    在Person中加入测试代码

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,10 @@
 public class Person {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("文开九运");
+        System.out.println("文开九运");
+        System.out.println("文开九运");
+        System.out.println("文开九运");
+        System.out.println("文开九运");
     }
 }
